04638a0977b12bca3aa23392f8195d49fae4e0c9
SpearJin <ckdwls2525@gmail.com>
2022-01-27 19:07:46 +0900

    FontAwesome 설정

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -3,9 +3,13 @@ import React from 'react';
 const Cart = (props) => (
   <div>
     <span>책상</span>
-    <button>-</button>
+    <button>
+      <i class='fas fa-minus'></i>
+    </button>
     <span>5</span>
-    <button>+</button>
+    <button>
+      <i class='fas fa-plus'></i>
+    </button>
     <span>150000원</span>
   </div>
 );
