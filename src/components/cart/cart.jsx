7c4b4c6987cac658da74b79b06020d6eff094c83
SpearJin <ckdwls2525@gmail.com>
2022-01-27 19:20:51 +0900

    cart 수량 감소&증가 구현

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Cart = (props) => (
-  <div>
-    <span>책상</span>
-    <button>
-      <i class='fas fa-minus'></i>
-    </button>
-    <span>5</span>
-    <button>
-      <i class='fas fa-plus'></i>
-    </button>
-    <span>150000원</span>
-  </div>
-);
+const Cart = (props) => {
+  const [quentity, setQuentity] = useState(0);
+
+  const changeQuentiy = (e) => {
+    const target = e.target;
+    if (target.closest('.minus')) {
+      const count = quentity === 0 ? 0 : quentity - 1;
+      setQuentity(count);
+    } else {
+      const count = quentity >= 9 ? 9 : quentity + 1;
+      setQuentity(count);
+    }
+  };
+
+  return (
+    <div>
+      <span>책상</span>
+      <button className='minus' onClick={changeQuentiy}>
+        <i className='fas fa-minus'></i>
+      </button>
+      <span>{quentity}</span>
+      <button className='plus' onClick={changeQuentiy}>
+        <i className='fas fa-plus'></i>
+      </button>
+      <span>150000원</span>
+    </div>
+  );
+};
 
 export default Cart;
