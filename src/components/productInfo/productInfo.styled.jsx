69b2134373298376b60af34b50e2a19f7ffcc67e
SpearJin <ckdwls2525@gmail.com>
2022-02-03 17:24:45 +0900

    productInfo 스타일 수정

--- src/components/productInfo/productInfo.styled.jsx
+++ src/components/productInfo/productInfo.styled.jsx
@@ -4,13 +4,17 @@ export const StyledProductInfo = styled.li`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 80%;
-  padding: 1em;
-  margin: 1em 0;
+  width: 40%;
+  padding: 0.5em 3em;
+  margin-top: 1em;
+  border-radius: 12px;
+  border: 1px solid lightgray;
   cursor: pointer;
   & > img {
     width: 200px;
     height: 200px;
+    border-radius: 20px;
+    object-fit: cover;
   }
   & > .product_description {
     display: flex;
